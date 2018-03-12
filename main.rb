cb3eedf0d4ab8cb5c041f12287a83e576e3517d4
Anikram <4nikram@gmail.com>
2018-03-12 15:17:33 +0300

    New main

--- main.rb
+++ main.rb
@@ -9,35 +9,11 @@ collection = ProductCollection.read_folders("#{__dir__}/data")
 collection.sort!(by: :price, order: :asc)
 
 sum = 0
-<<<<<<< HEAD
-=======
 sold_products = []
->>>>>>> issue
 puts "Добро пожаловать в магазин \"Диски, фильмы, и книги\"\n\n"
 puts "Выберите товар (для выхода введите \"0\"):"
 user_input = -1
 
-<<<<<<< HEAD
-until input == 0
-  correct_choices = [0]
-  puts "Выберите товар: (для завершения выберите \"0\") \n"
-
-   collection.to_a.each_with_index do |product, index|
-     puts "#{index + 1}. #{product.to_s}"
-     correct_choices << index + 1
-   end
-
-  puts "0. Выход"
-  input = STDIN.gets.to_i
-
-  if input == 0
-    break
-  elsif correct_choices.include?(input)
-    sum += collection.to_a[input - 1].price.to_i
-    collection.to_a[input - 1].sell_an_item
-    puts "Вы выбрали на #{sum} рублей"
-    collection.to_a.delete_at(input - 1) if collection.to_a[input - 1].quantity == 0
-=======
 until user_input == 0 || !collection.is_empty?
   puts "\nВыбрано товаров на #{sum} руб.\n"
   puts collection.display
@@ -48,17 +24,11 @@ until user_input == 0 || !collection.is_empty?
   if reply.is_a?(Hash)
     sum += reply[:price]
     sold_products << reply[:title]
->>>>>>> issue
   else
     puts reply
   end
 end
 
-<<<<<<< HEAD
-result = 'Жаль что вы ничего не купили. Приходите в следующий раз!'
-result = "С Вас #{sum} рублей. Спасибо за заказ!" if sum > 0
-puts result
-=======
 puts "------------"
 
 if sum > 0
@@ -72,4 +42,3 @@ if sum > 0
 else
   puts 'Жаль что вы ничего не купили. Приходите в следующий раз!'
 end
->>>>>>> issue
